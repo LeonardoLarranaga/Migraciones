0d7b23861830c3ce1d3d071dc0e4e1bfd8aaa1c9
Leonardo Larrañaga <leonardolarranaga@icloud.com>
2024-04-10 22:37:16 -0700

    Git init

---
+++ migrations/20240409181143-create-activo.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Activos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      numSerie: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true
+      },
+      numInventario: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true
+      },
+      descripcion: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      imagen: {
+        type: Sequelize.BLOB,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Activos');
+  }
+};
---
+++ models/responsable.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Responsable extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Responsable.init({
+    numeroEmpleado: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    imagen: {
+      type: DataTypes.INTEGER,
+    }
+  }, {
+    sequelize,
+    modelName: 'Responsable',
+  });
+  return Responsable;
+};
